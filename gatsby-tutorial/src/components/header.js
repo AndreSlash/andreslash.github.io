4742cfd3469ea883df2513f3024c8307406bfb10
unknown <aserafin4@wp.pl>
2020-11-11 17:54:43 +0100

    Adding basic css to the project

--- gatsby-tutorial/src/components/header.js
+++ gatsby-tutorial/src/components/header.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import {Link} from 'gatsby'
+// import './header.module.scss'
+import headerStyles from './header.module.scss'
 export const header = () => {
     return (
-        <header>
-            Welcome 
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-                <li><Link to="/blog">Blog</Link></li>
-                <li><Link to="/about">About</Link></li>
+        <header className={headerStyles.header}>
+            <h1>
+                <Link className={headerStyles.title} to="/">
+                    Andrzej Serafin
+                </Link>
+            </h1>
+            <ul className={headerStyles.navList}>
+                <li><Link className={headerStyles.navItem} to="/">Home</Link></li>
+                <li><Link className={headerStyles.navItem} to="/contact">Contact</Link></li>
+                <li><Link className={headerStyles.navItem} to="/blog">Blog</Link></li>
+                <li><Link className={headerStyles.navItem} to="/about">About</Link></li>
             </ul>
         </header>
     )
--- gatsby-tutorial/src/components/layout.js
+++ gatsby-tutorial/src/components/layout.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../components/header'
 import Footer from '../components/footer'
+import '../styles/index.scss'
+import layoutStyles from './layout.module.scss'
 export const layout = (props) => {
     return (
-        <div>
-            <Header></Header>
+        <div className={layoutStyles.container}>
+            <div className={layoutStyles.content}>
+                <Header></Header>
                 {props.children}
+            </div>
             <Footer></Footer>
         </div>
     )
